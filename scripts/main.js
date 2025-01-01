668d8489d814533a25e3bba8861ef46e69d0be2c
ReLoadXx <02ReloadX@gmail.com>
2025-01-01 02:19:46 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+let input=""
+
+function appNumero(num) {
+    input += num;
+    document.getElementById("display").value = input
+}
+
+function appOperador(operador) {
+    input += "" + operador + "";
+    document.getElementById("display").value = operador
+}
+
+function calcular() {
+    try {
+        let resultado = eval(input);
+        document.getElementById("display").value= resultado;
+        input = resultado
+        
+    } catch {
+        document.getElementById("display").value = "Error";
+        input = "";
+    }
+}
+
+function reset() {
+    document.getElementById("display").value = "0";
+    input = "";
+}
